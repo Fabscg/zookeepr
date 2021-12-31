5a5222f59a88b64afc68330a24f41a30abffe7b7
Fabulosa <fabiscg79@gmail.com>
2021-12-31 13:05:59 -0500

    Finish GET Requests to Serve Page Data

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 const $animalForm = document.querySelector('#animals-form');
 const $displayArea = document.querySelector('#display-area');
 
@@ -27,11 +29,20 @@ const getAnimals = (formData = {}) => {
 
   Object.entries(formData).forEach(([key, value]) => {
     queryUrl += `${key}=${value}&`;
-  });
-
+  })
   console.log(queryUrl);
-
 };
+fetch(queryUrl)
+  .then(response => {
+    if (!response.ok) {
+      return alert('Error: ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(animalData => {
+    console.log(animalData);
+    printResults(animalData);
+  });
 
 const handleGetAnimalsSubmit = event => {
   event.preventDefault();
